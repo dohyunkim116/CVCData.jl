0b382b55cfeb3215548d0a1d7988baebcc0978fc
Do Hyun Kim <dkim116@ucla.edu>
2025-11-13 07:07:41 -0800

    Add Nsubset parameter to simulate_cov function for optional subsetting of rows.

--- src/Covariate.jl
+++ src/Covariate.jl
@@ -5,12 +5,21 @@ function simulate_cov(
     N::Int,
     C::Int,
     covdir::AbstractString,
-    covfile_basename::AbstractString="w"
+    covfile_basename::AbstractString="w";
+    Nsubset::Union{Nothing, Int}=nothing
     )
     Random.seed!(rng)
     mkpath(covdir)
     covfile_df = covfile_basename * "df.txt"
     w = [string.(1:N) ones(N) randn(N, C - 1)];
+    if Nsubset !== nothing
+        if Nsubset > N
+            @warn "Requested subset size Nsubset=$(Nsubset) is larger than total N=$(N). Using full sample."
+            Nsubset = N
+        end
+        selected_rows = shuffle(rng, 1:N)[1:Nsubset]
+        w = w[selected_rows, :]
+    end
     wdf = DataFrame(w, ["f.eid"; "intercept"; string.(1:C-1)])
     CSV.write("$covdir/$(covfile_df)", wdf; delim = "\t", quotestrings = true, header=true)
     
